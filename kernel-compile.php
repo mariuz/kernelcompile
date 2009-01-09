24c4eaddb807f6dedccdd7ec3faa4765c763a679
mariuz <mapopa@gmail.com>
2009-01-09 10:36:21 +0200

    added the script and the doc

---
+++ kernel-compile.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?
+$kernel_version = "2.6.28";
+passthru("chmod +x /usr/bin/php-fastcgi");
+chdir("/usr/src");
+passthru("wget --continue http://kernel.org/pub/linux/kernel/v2.6/linux-$kernel_version.tar.bz2");
+chdir("linux-$kernel_version");
+passthru("cp /boot/config-`uname -r` ./.config");
+passthru("make oldconfig");
+passthru("make kpkg-clean");
+passthru("fakeroot make-kpkg --initrd --append-to-version=-vanillaice kernel_image kernel_headers");
+?>
