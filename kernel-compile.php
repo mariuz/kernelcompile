cb1e66d993752769bf09e10775211eab1c9ed5f3
Marius Popa <mapopa@gmail.com>
2009-01-19 09:45:41 +0200

    fixed the make-kpkg clean

--- kernel-compile.php
+++ kernel-compile.php
@@ -6,7 +6,7 @@ passthru("wget --continue http://kernel.org/pub/linux/kernel/v2.6/linux-$kernel_
 passthru("tar -jxf linux-$kernel_version.tar.bz2");
 chdir("linux-$kernel_version");
 passthru("cp /boot/config-`uname -r` ./.config");
-passthru("make oldconfig");
-passthru("make kpkg-clean");
+passthru("make menuconfig");
+passthru("make-kpkg clean");
 passthru("fakeroot make-kpkg --initrd --append-to-version=-vanillaice kernel_image kernel_headers");
 ?>
