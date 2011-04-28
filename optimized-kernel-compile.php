eea0054789e0ba845aaec16ca88d2ba444ec8cf6
Popa Marius Adrian <mapopa@gmail.com>
2011-04-28 12:51:24 +0300

    fix make localmodconfig

--- optimized-kernel-compile.php
+++ optimized-kernel-compile.php
@@ -13,8 +13,8 @@ chdir("/usr/src");
 passthru("wget --continue http://kernel.org/pub/linux/kernel/v2.6/linux-$kernel_version.tar.bz2");
 passthru("tar -jxf linux-$kernel_version.tar.bz2");
 chdir("linux-$kernel_version");
-passthru("cp /boot/config-`uname -r` ./.config");
-passthru("make make localmodconfig");
+//passthru("cp /boot/config-`uname -r` ./.config");
+passthru("make localmodconfig");
 passthru("make-kpkg clean");
 passthru("CONCURRENCY_LEVEL=$cpuCount fakeroot make-kpkg --initrd --append-to-version=-vanillaice kernel_image kernel_headers");
 ?>
