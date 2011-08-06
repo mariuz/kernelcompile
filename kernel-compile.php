5ae0676023517b33a470dd319fd45168b48bd386
Marius Popa <mapopa@gmail.com>
2011-08-06 16:59:04 +0300

    Added BUILD_DIR constant and make clean after all is finished

--- kernel-compile.php
+++ kernel-compile.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php
 <?
 $KERNEL_URL='http://www.kernel.org/';
+$BUILD_DIR="/usr/src";
 include_once('./getKernelVersion.php');
 include_once('./getCpuCount.php');
 
@@ -9,7 +10,7 @@ $cpuCount=getCpuCount();
 print("cpu count:$cpuCount\n");
 print("compiling kernel $kernel_version\n");
 passthru("apt-get install kernel-package git-core fakeroot ncurses-dev");
-chdir("/usr/src");
+chdir($BUILD_DIR);
 passthru("wget --continue http://kernel.org/pub/linux/kernel/v3.0/linux-$kernel_version.tar.bz2");
 passthru("tar -jxf linux-$kernel_version.tar.bz2");
 chdir("linux-$kernel_version");
@@ -17,4 +18,5 @@ passthru("cp /boot/config-`uname -r` ./.config");
 passthru("make menuconfig");
 passthru("make-kpkg clean");
 passthru("CONCURRENCY_LEVEL=$cpuCount fakeroot make-kpkg --initrd --append-to-version=-vanillaice kernel_image kernel_headers");
+passthru("make clean");
 ?>
