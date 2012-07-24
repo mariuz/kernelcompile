7abc7ceea76c3fa809b8f19611f00d421aa2d590
Popa Adrian Marius <mapopa@gmail.com>
2012-07-24 16:58:31 +0300

    cleanup

---
+++ functions.php
@@ -0,0 +1,48 @@
+<?php
+include_once('./simplehtmldom/simple_html_dom.php');
+function GetStableVersion()
+{
+// create HTML DOM
+    $html = file_get_html('http://www.kernel.org/kdist/version.shtml');
+    foreach($html->find('tr') as $row) {
+      foreach($row->find('td') as $td)
+       {
+        if ($td->innertext=='stable:')
+        {
+        	$kernel_version=$row->find('strong', 0)->innertext;
+        	$ret = $kernel_version;
+         	// clean up memory
+        	$html->clear();
+        	unset($html);
+        	return $ret;
+        }
+        }
+       }
+
+}
+function GetReleaseCandidateVersion()
+{
+  $html = file_get_html('http://www.kernel.org/kdist/version.shtml');
+    foreach($html->find('tr') as $row) {
+      foreach($row->find('td') as $td)
+       {
+        if ($td->innertext=='mainline:')
+        {
+        $kernel_version=$row->find('strong', 0)->innertext;
+        $ret = $kernel_version;
+         // clean up memory
+        $html->clear();
+        unset($html);
+        return $ret;
+        }
+        }
+       }
+
+}
+function getCpuCount()
+{
+  exec("grep -c  processor /proc/cpuinfo",&$out);
+  return $out[0];
+
+}
+?>
--- getCpuCount.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getCpuCount()
-{
-  exec("grep -c  processor /proc/cpuinfo",&$out);
-  return $out[0];
-
-}
-?>
