08c1c58c133fbbd92f7891ec93573694b67ef5a3
Popa Marius Adrian <mapopa@gmail.com>
2013-03-05 09:28:29 +0200

    Update kernel-compile.php

--- kernel-compile.php
+++ kernel-compile.php
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import subprocess
 KERNEL_URL='http://www.kernel.org/'
 BUILD_DIR="/usr/src"
 include_once('./functions.php')
@@ -8,8 +8,8 @@ kernel_version=GetStableVersion(KERNEL_URL)
 cpuCount=getCpuCount()
 print("cpu count:$cpuCount\n")
 print("compiling kernel $kernel_version\n")
-passthru("apt-get install git-core kernel-package fakeroot build-essential ncurses-dev")
-chdir($BUILD_DIR)
+subprocess.Popen("apt-get install git-core kernel-package fakeroot build-essential ncurses-dev",shell=True)
+chdir(BUILD_DIR)
 passthru("wget --continue http://kernel.org/pub/linux/kernel/v3.x/linux-$kernel_version.tar.bz2")
 passthru("tar -jxf linux-$kernel_version.tar.bz2")
 chdir("linux-$kernel_version")
