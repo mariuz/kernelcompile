8450cfea0f227ed6aa321d6e07a2ac05aac0f854
Popa Marius Adrian <mapopa@gmail.com>
2013-03-05 09:18:17 +0200

    Update kernel-compile.php

--- kernel-compile.php
+++ kernel-compile.php
@@ -1,22 +1,22 @@
-#!/usr/bin/php
-<?
-$KERNEL_URL='http://www.kernel.org/';
-$BUILD_DIR="/usr/src";
-include_once('./functions.php');
+#!/usr/bin/python
 
-$kernel_version=GetStableVersion($KERNEL_URL);
-$cpuCount=getCpuCount();
-print("cpu count:$cpuCount\n");
-print("compiling kernel $kernel_version\n");
-passthru("apt-get install git-core kernel-package fakeroot build-essential ncurses-dev");
-chdir($BUILD_DIR);
-passthru("wget --continue http://kernel.org/pub/linux/kernel/v3.x/linux-$kernel_version.tar.bz2");
-passthru("tar -jxf linux-$kernel_version.tar.bz2");
-chdir("linux-$kernel_version");
-passthru("cp /boot/config-`uname -r` ./.config");
-passthru("make menuconfig");
-passthru("make-kpkg clean");
+KERNEL_URL='http://www.kernel.org/'
+BUILD_DIR="/usr/src"
+include_once('./functions.php')
+
+kernel_version=GetStableVersion(KERNEL_URL)
+cpuCount=getCpuCount()
+print("cpu count:$cpuCount\n")
+print("compiling kernel $kernel_version\n")
+passthru("apt-get install git-core kernel-package fakeroot build-essential ncurses-dev")
+chdir($BUILD_DIR)
+passthru("wget --continue http://kernel.org/pub/linux/kernel/v3.x/linux-$kernel_version.tar.bz2")
+passthru("tar -jxf linux-$kernel_version.tar.bz2")
+chdir("linux-$kernel_version")
+passthru("cp /boot/config-`uname -r` ./.config")
+passthru("make menuconfig")
+passthru("make-kpkg clean")
 passthru("CONCURRENCY_LEVEL=$cpuCount fakeroot make-kpkg --initrd --append-to-version=-vanillaice kernel_image kernel_headers");
-passthru("make clean");
-Install($kernel_version);
-?>
+passthru("make clean")
+Install($kernel_version)
+
