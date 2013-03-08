b6a9cad7addd21de7b00f03bc5987d5f999240a6
Popa Marius Adrian <mapopa@gmail.com>
2013-03-08 12:46:54 +0200

    replace Popen with call

--- kernel-compile.py
+++ kernel-compile.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 from subprocess import *
 from os import chdir
-Popen("apt-get install git-core kernel-package fakeroot build-essential ncurses-dev python-pip wget",shell=True)
-Popen("pip install feedparser",shell=True)
+from sh import cp,make,uname
+#call("apt-get install git-core kernel-package fakeroot build-essential ncurses5-dev python-pip wget",shell=True)
+#call("pip install feedparser",shell=True)
 from functions.functions import *
 BUILD_DIR="/usr/src"
 kernel_version=GetStableVersion()
@@ -11,13 +12,14 @@ print("cpu count:%s\n" % cpuCount)
 print("compiling kernel %s\n" % kernel_version)
 
 chdir(BUILD_DIR)
-Popen("wget --continue http://kernel.org/pub/linux/kernel/v3.x/linux-%s.tar.bz2" % kernel_version,shell=True)
-Popen("tar -jxf linux-%s.tar.bz2" % kernel_version,shell=True)
+#Popen("wget --continue http://kernel.org/pub/linux/kernel/v3.x/linux-%s.tar.bz2" % kernel_version,shell=True)
+#Popen("tar -jxf linux-%s.tar.bz2" % kernel_version,shell=True)
 chdir("linux-%s" % kernel_version)
-Popen("cp /boot/config-`uname -r` ./.config",shell=True)
-Popen("make menuconfig",shell=True)
-Popen("make-kpkg clean",shell=True)
-Popen("CONCURRENCY_LEVEL=%s fakeroot make-kpkg --initrd --append-to-version=-vanillaice kernel_image kernel_headers" % cpuCount,shell=True)
-Popen("make clean",shell=True)
+current_kernel=uname("-r").rstrip('\n')
+cp("/boot/config-%s"%current_kernel,"./.config")
+call("make nconfig",shell=True)
+call("make-kpkg clean",shell=True)
+call("CONCURRENCY_LEVEL=%s fakeroot make-kpkg --initrd --append-to-version=-vanillaice kernel_image kernel_headers" % cpuCount,shell=True)
+call("make clean",shell=True)
 #Install(kernel_version)
 
