e00129a3c810aa03083e42b6b670856ba3fe3a7b
Popa Marius Adrian <mapopa@gmail.com>
2013-03-11 06:51:25 +0200

    uncomments

--- kernel-compile.py
+++ kernel-compile.py
@@ -2,8 +2,8 @@
 from subprocess import *
 from os import chdir
 from sh import cp,make,uname
-#call("apt-get install git-core kernel-package fakeroot build-essential ncurses5-dev python-pip wget",shell=True)
-#call("pip install feedparser",shell=True)
+call("apt-get install git-core kernel-package fakeroot build-essential ncurses5-dev python-pip wget",shell=True)
+call("pip install feedparser",shell=True)
 from functions.functions import *
 BUILD_DIR="/usr/src"
 kernel_version=GetStableVersion()
@@ -12,8 +12,8 @@ print("cpu count:%s\n" % cpuCount)
 print("compiling kernel %s\n" % kernel_version)
 
 chdir(BUILD_DIR)
-#Popen("wget --continue http://kernel.org/pub/linux/kernel/v3.x/linux-%s.tar.bz2" % kernel_version,shell=True)
-#Popen("tar -jxf linux-%s.tar.bz2" % kernel_version,shell=True)
+Popen("wget --continue http://kernel.org/pub/linux/kernel/v3.x/linux-%s.tar.bz2" % kernel_version,shell=True)
+Popen("tar -jxf linux-%s.tar.bz2" % kernel_version,shell=True)
 chdir("linux-%s" % kernel_version)
 current_kernel=uname("-r").rstrip('\n')
 cp("/boot/config-%s"%current_kernel,"./.config")
