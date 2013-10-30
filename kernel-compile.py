2dd051b8c243d053c9df4f08c8b022e61c686f6f
screwgoth <raseel.bhagat@zymrinc.com>
2013-10-30 18:43:45 +0530

    Validation for 'type' argument

--- kernel-compile.py
+++ kernel-compile.py
@@ -5,6 +5,15 @@ import sys, getopt
 from os import chdir
 from functions.functions import *
 
+def validate_kernel_type(kernel_type):
+  if(len(kernel_type) > 0):
+    if ((kernel_type == "stable") or (kernel_type == "mainline") or (kernel_type == "lts")):
+      return True
+    else:
+      return False
+  else:
+    return False
+
 def main(argv):
   opts, args = getopt.getopt(argv,"ht:v:",["help","type=","version="])
   kernel_type = 'stable'
@@ -14,7 +23,13 @@ def main(argv):
          print 'kerenelcompile.py -type <stable|mainline|lts> -v <version>'
          sys.exit()
       elif opt in ("-t", "--type"):
-         kernel_type = arg
+         isValidKernelType = validate_kernel_type(arg)
+         if(isValidKernelType == True):
+             kernel_type = arg
+         else:
+             print("Invalid or No Kernel type entered")
+             print("Valid options for the 'type' argument are : 'stable' or 'mainline' or 'lts'\n")
+             sys.exit()
       elif opt in ("-v", "--version"):
          version = arg
   call("apt-get install git-core kernel-package fakeroot build-essential libncurses5-dev python-pip wget xz-utils",shell=True)
